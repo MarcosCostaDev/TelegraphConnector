f88c3543e74eca3f7e8a6f7908ecb946713ae01e
Marcos Costa <marcos.lcosta@live.com>
2022-12-18 18:42:18 -0500

    feat: deployment of packages to GITHUB and nuget

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Telegraph Connector!
+
+[![Build](https://github.com/MarcosCostaDev/TelegraphConnector/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/MarcosCostaDev/TelegraphConnector/actions/workflows/build-and-test.yml)
--- src/TelegraphConnector/AssemblyInfo.cs
+++ src/TelegraphConnector/AssemblyInfo.cs
@@ -15,9 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarcosCostaDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("MarcosCostaDev © 2022")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n      Telegra.ph API for .net\r\n      This package allow developers to connect t" +
-    "he Telegra.ph API and use its features for create beautiful pages in the Telegra" +
-    "m component\r\n    ")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute(@"Introducing TelegraphConnector - the Nuget package that makes it easy to communicate with the Telegra.ph API. 
+With this package, you can create a web page using resources from Telegra.ph, giving you access to a wide range of features and functionality. 
+Whether you want to publish articles, or share multimedia content, TelegraphConnector makes it simple to do so. 
+Simply install the package, create or authenticate with your Telegra.ph account, and start building your web page for using the instant view in the Telegram.  
+With easy-to-use methods and comprehensive documentation, TelegraphConnector is the perfect choice for anyone looking to utilize the power of Telegra.ph.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegraphConnector")]
